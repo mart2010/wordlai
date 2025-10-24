d46bffee76ea522288e5a4d00492d19ec6f612e1
Martin Ouellet <martin.ouellet@vd.ch>
2025-10-24 18:35:36 +0200

    Moving forward

--- src/domain.py
+++ src/domain.py
@@ -19,44 +19,6 @@ def generate_puzzle_id() -> str:
     return f"{now.year:04d}{now.month:02d}{now.day:02d}{seconds_since_midnight:05d}"
 
 
-
-# @dataclass
-# class WordSpec:
-#     word: str
-#     clue: str
-#     # starting first row and col
-#     row: int = None
-#     col: int = None
-#     # 0='across', 1='down'
-#     direction: int = None
-    
-# @dataclass
-# class PuzzleSpec:
-#     id: str = field(default_factory=generate_puzzle_id)
-#     rows: int = 0
-#     cols: int = 0
-#     words: List[WordSpec] = field(default_factory=list)
-#     # Puzzle sequential number stored/persisted in Store 
-#     no: int = None
-
-#     def to_dict(self):
-#         return {
-#             "id": self.id,
-#             "rows": self.rows,
-#             "cols": self.cols,
-#             "words": [w.__dict__ for w in self.words]
-#         }
-
-#     @staticmethod
-#     def from_dict(data):
-#         return PuzzleSpec(
-#             id=data["id"],
-#             rows=data["rows"],
-#             cols=data["cols"],
-#             words=[WordSpec(**w) for w in data["words"]]
-#         )
-
-
 def count_chars(s: str) -> int:
     """Count letters in s, return (nb_chars, firstchar_pos, lastchar_pos)
     string s is expected to contain letters and '-' as fillable markers.
@@ -102,10 +64,10 @@ def get_regex(letter_seq: str):
     result = ''.join(result_list) + pfix
     return result
 
-def gen_patterns(letter_seq: str, pos): 
-    """Generate all possible regex patterns holding at least one letter 
-    from the given letter sequence, starting at position pos in grid 
-    (i.e. where we can begin to fill the grid) 
+def gen_patterns(letter_seq: str, pos, min_size): 
+    """Generate all regex patterns and sub-patterns from letter_seq 
+    (with 1 or more letters) of size larger or equal to min_size.
+    Where pos indicates position in grid 
 
     Args:
         letter_seq: letters pattern, e.g. '--R----G--E'
@@ -113,19 +75,23 @@ def gen_patterns(letter_seq: str, pos):
         
     Yields: Tuples of (pattern_regex, pos)
     """
-    yield (get_regex(letter_seq), pos)
     nb_chars, first_char_pos, last_char_pos = count_chars(letter_seq)
+    if len(letter_seq) < min_size:
+        return
     
+    yield (get_regex(letter_seq), pos)
+    
+    # generate recursively sub-patterns
     if nb_chars > 1:
         # trim "right":
         sub_seq = letter_seq[:last_char_pos-1]
         if len(sub_seq) > 1:
-            yield from gen_patterns(sub_seq, pos=pos)
+            yield from gen_patterns(sub_seq, pos, min_size)
         # trim "left"
         new_pos = pos + first_char_pos + 2
         sub_seq = letter_seq[first_char_pos+2:]
         if len(sub_seq) > 1:
-            yield from gen_patterns(sub_seq, pos=new_pos)
+            yield from gen_patterns(sub_seq, new_pos, min_size)
 
 
 @dataclass(order=True)
@@ -146,13 +112,13 @@ class Word:
         self.canonical = self.word.upper()
         self.size = len(self.canonical)
     
-    def place_word(self, row: int, col: int, direction: int):
+    def set_position(self, row: int, col: int, direction: int):
         self.start_row = row
         self.start_col = col
         self.direction = direction
     
     def blocked_span(self, padding=False) -> tuple[int,int]:
-        """Return blocked span of this word optionnally encompassing 
+        """Return blocked span of this word, optionnally encompassing 
         one letter before and after the word itself (padding=True).
         """
         if self.start_row is None or self.start_col is None or self.direction is None:
@@ -189,7 +155,7 @@ class Puzzle:
         """
         self.id: str = generate_puzzle_id()
         self.grid_size = grid_size      
-        self.available_words: List[Word] = []
+        self.available_words: list[Word] = []
         self.min_size_word = self.grid_size
         for i, w in enumerate(available_words):
             if len(w[0]) < self.min_size_word:
@@ -204,23 +170,28 @@ class Puzzle:
         self.no: int = None
         
         # {direction: {col/row-index: [Wordx, ...]}}
-        self.placed_words: Dict[int, Dict[int, List[Word]]] = {}
-        # {direction: [col/row-indexes]}, initially all grid available
+        self.placed_words: dict[int, dict[int, list[Word]]] = {}
+        
+        # 2 convenient structures used for optimization {direction: [col/row-indexes]}
+        # initially no index is blocked
         self.blocked_indexes: dict[int:list[int]] = {0:[], 1:[]}
+        # and all indexes empty
+        self.empty_indexes: dict[int:list[int]] = {0:list(range(self.grid_size)), 
+                                                   1:list(range(self.grid_size))}
 
 
-    def derive_letter_sequences(self, direction: int, index: int) -> tuple[list[str], list[str]]:
-        """Derive letter sequences for given row/col index according to direction with '-' fillable markers, 
-        '0' blocked markers, and 'L' letter from already perpendicular placed words.
+    def get_letter_sequences(self, direction: int, index: int) -> tuple[list[str], list[str]]:
+        """Derive letter sequences for given row/col index and direction, 
+        with '-' fillable markers, '0' blocked markers, and 'X' letter from perpendicular placed words.
         """
-        frozen_spans = [w.blocked_span(padding=True) for w in self.placed_words.get(direction, {}).get(index, [])]
-        left_frozen_spans = [w.blocked_span() for w in self.placed_words.get(direction, {}).get(index-1, [])]
-        right_frozen_spans = [w.blocked_span() for w in self.placed_words.get(direction, {}).get(index+1, [])]
+        block_spans = [w.blocked_span(padding=True) for w in self.placed_words.get(direction, {}).get(index, [])]
+        left_block_spans = [w.blocked_span() for w in self.placed_words.get(direction, {}).get(index-1, [])]
+        right_block_spans = [w.blocked_span() for w in self.placed_words.get(direction, {}).get(index+1, [])]
 
         # fillout cells with '-' (fillable marker)
         l_seq = ['-' for _ in range(self.grid_size)]
         # overwrite cells within blocked spans
-        for span in frozen_spans + left_frozen_spans + right_frozen_spans:
+        for span in block_spans + left_block_spans + right_block_spans:
             l_seq[span[0]:span[1]] = ['0' for _ in range(span[0], span[1])]
 
         # overwrite cells with letters from perpendicular words
@@ -228,21 +199,64 @@ class Puzzle:
             if l == '-':
                 index_c = index if direction == 1 else i
                 index_r = i if direction == 1 else index
-                letter_crossed = [w.letter_at(index_r, index_c) for w in self.placed_words.get(1 - direction, {}).get(i, []) if w.letter_at(index_r, index_c)]
+                letter_crossed = [w.letter_at(index_r, index_c) 
+                                  for w in self.placed_words.get(1 - direction, {}).get(i, []) if w.letter_at(index_r, index_c)]
                 assert len(letter_crossed) <= 1
                 if len(letter_crossed) == 1:
                     l_seq[i] = letter_crossed[0]
+        if l_seq.count('-') == self.grid_size:
+            raise Exception(f"No letter found in direction={direction} and index={index}")
+        
+        # keep only consecutive pattern (unblocked) of min size
+        l_seq_string = ''.join(l_seq)
+        l_consecutive_tuple = [(p,l_seq_string.index(p)) for p in l_seq_string.split('0') if len(p) >= self.min_size_word]
+        
+        # return tuple of (pattern, position) sorted from largest to smallest in pattern length
+        return sorted(l_consecutive_tuple, key=lambda x: len(x[0]), reverse=True)
 
-        return ''.join(l_seq)
- 
 
-    def fillout_grid(self):
-        """Randonly select a next row or col, get its letter_sequence consecutive pattens
+    def solve(self, timeout: int = 60):
+        """Randonly select a not empty and not filled row or col, get its letter_sequence consecutive pattens
           and try to find a matching word
         
         Shoud probably prioritize the one with longest letter_seq available!!
         ..to be experimented!
         """
+        self.place_first_word()
+        # randomly fill out rows/cols until blocked or timeout
+        start_time = time.time()
+        while not self.is_blocked() or time.time() - start_time < timeout:
+            direction = random.choice([0,1])
+            available_indexes = [i for i in range(self.grid_size) 
+                                 if i not in self.blocked_indexes[direction] and i not in self.empty_indexes[1-direction]]
+            if len(available_indexes) == 0:
+                direction = 1 - direction
+                available_indexes = [i for i in range(self.grid_size) if i not in self.blocked_indexes[direction]]
+                assert len(available_indexes) > 0
+            
+            #TODO complete logic here .. 
+            index = random.choice(available_indexes)
+            letter_seqs = self.derive_letter_sequences(direction, index).split('0')
+            
+            for letter_seq in letter_seqs:
+                if len(letter_seq) < self.min_size_word:
+                    continue
+
+                complete_index = True
+                for p_regex, pos in self.gen_patterns(letter_seq, pos=0):
+                    nb_chars, first_char_pos, last_char_pos = count_chars(letter_seq)
+                    if nb_chars >= self.min_size_word:
+                        match_result = self.find_matches(letter_seq)
+                        if match_result:
+                            word_n, matched_word = match_result
+                            if direction == 0:
+                                self.place_word(word_n=word_n, row=index, col=pos, direction=direction)
+                            else:
+                                self.place_word(word_n=word_n, row=pos, col=index, direction=direction)
+                            complete_index = False
+                            break
+                if complete_index:
+                    self.blocked_indexes[direction].append(index)
         
         # select randon col or row not in self.blocked_indexes
   
@@ -276,6 +290,23 @@ class Puzzle:
                 matched_word = match.group(2)
                 return word_n, matched_word
 
+    def place_first_word(self):
+        """"place first word pickin randomly top-5 longest word
+        """
+        if len(self.placed_words) == 0:    
+            first_w_i = random.randint(0, 4)
+            first_w_len = len(self.available_words[first_w_i].canonical)
+            assert first_w_len <= self.grid_size
+            index = random.randint(0, self.grid_size - 1)
+            other_index = random.randint(0, self.grid_size - first_w_len)
+            direction = random.choice([0,1])
+            if direction == 0:
+                self.place_word(word_n=first_w_i, row=index, col=other_index, direction=direction)
+            else:
+                self.place_word(word_n=first_w_i, row=other_index, col=index, direction=direction)
+
+
+
     def place_word(self, word_n: int, row: int, col: int, direction: int):
         """Place word identified by word_n at given row, col, direction in grid.
 
@@ -286,31 +317,45 @@ class Puzzle:
             direction: 0=across, 1=down
         """
         word = self.available_words[word_n]
-        word.place_word(row, col, direction)
+        word.set_position(row, col, direction)
         self.placed_words.setdefault(direction, {}).setdefault((col if direction==1 else row), []).append(word)
         # refresh available_wordseq 
+        s_index = self.available_wordseq.index(f'[{word_n}]')
+        e_index = self.available_wordseq.find('[', start=s_index+1)
+        if e_index == -1:
+            e_index = len(self.available_wordseq)
+        self.available_wordseq = self.available_wordseq[:s_index] + self.available_wordseq[e_index:]
+        # no need to refresh self.blocked_indexes as this is done during solve() (index can be not full but no word match is found)
 
-        # refresh self.blocked_indexes according to row/col having no perpendicular words placed yet 
+        # refresh self.empty_indexes
+        [ self.empty_indexes(1-direction).remove(i) for i in self.empty_indexes(1-direction) if i in word.blocked_span() ]
 
 
-    # probably not needed
-    def cell_status(self, row: int, col: int, direction):
-        """Return status hint at given cell (row, col) according to direction:
-        Returns:
+    def is_blocked(self) -> bool:
+        """Return True if puzzle can no longer be filled (all rows and cols blocked).
         """
-        # 1st check word in same direction        
-        words_in_direction = self.placed_words.get(direction, {}).get(col if direction==1 else row, [])            
-        for w in words_in_direction:
-            if w.in_frozen_span(row, col):
-                return 0
-        # check if letter present from words in other direction
-        other_direction = 1 - direction
-        words_in_other_direction = self.placed_words.get(other_direction, {}).get(col if other_direction==1 else row, [])
+        return (len(self.blocked_indexes[0]) == self.grid_size) and (len(self.blocked_indexes[1]) == self.grid_size)
         
+           
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "rows": self.rows,
+#             "cols": self.cols,
+#             "words": [w.__dict__ for w in self.words]
+#         }
 
-            
+#     @staticmethod
+#     def from_dict(data):
+#         return PuzzleSpec(
+#             id=data["id"],
+#             rows=data["rows"],
+#             cols=data["cols"],
+#             words=[WordSpec(**w) for w in data["words"]]
+#         )
 
-            
+    def __str__(self):
+        pass
 
 
 def generate_grid_pattern(current_puzzle: Puzzle, direction: int):
--- src/test_domain.py
+++ src/test_domain.py
@@ -2,7 +2,7 @@ import pytest
 import domain
 
 
-def test_count_chars():
+def tst_count_chars():
     try:
         ret = domain.count_chars('-------')
         assert False, "Expected ValueError for no letters"
@@ -17,7 +17,7 @@ def test_count_chars():
 
 
 
-def test_get_regex():
+def tst_get_regex():
     # en empty grid not valid (for 1st word is handled exceptionnally) 
     try:
         ret = domain.get_regex('-------')
@@ -32,8 +32,16 @@ def test_get_regex():
     assert ret == r'\[(\d+)\](F\w{1}ß\w{3}K\w{3}中\w{0,2})\[(\d+)\]'
 
 
-def test_gen_patterns():
-    patterns = list(domain.gen_patterns('--R----G--E', pos=0))
+def tst_gen_patterns():
+
+    try:
+        for p in domain.gen_patterns('------', 0, 2):
+            pass
+        assert False, "Expected ValueError for no letter"
+    except ValueError:
+        pass
+
+    patterns = list(domain.gen_patterns('--R----G--E', pos=0, min_size=2))
     expected_patterns = [
         (r'\[(\d+)\](\w{0,2}R\w{4}G\w{2}E)\[(\d+)\]', 0),  # '--R----G--E'
         (r'\[(\d+)\](\w{0,2}R\w{4}G\w{0,1})\[(\d+)\]',0),  # '--R----G-'
@@ -44,8 +52,15 @@ def test_gen_patterns():
     ]
     assert set(patterns) == set(expected_patterns)
 
+    patterns = list(domain.gen_patterns('--R----G--E', pos=0, min_size=8))
+    expected_patterns = [
+        (r'\[(\d+)\](\w{0,2}R\w{4}G\w{2}E)\[(\d+)\]', 0),  # '--R----G--E'
+        (r'\[(\d+)\](\w{0,2}R\w{4}G\w{0,1})\[(\d+)\]',0),  # '--R----G-'
+    ]
+    assert set(patterns) == set(expected_patterns)
 
-    patterns = list(domain.gen_patterns('F-ß---K---中--', pos=0))
+
+    patterns = list(domain.gen_patterns('F-ß---K---中--', pos=0, min_size=3))
     expected_patterns = [
         (r'\[(\d+)\](F\w{1}ß\w{3}K\w{3}中\w{0,2})\[(\d+)\]', 0), # 'F-ß---K---中--'
         (r'\[(\d+)\](F\w{1}ß\w{3}K\w{0,2})\[(\d+)\]', 0),        # 'F-ß---K--'
@@ -57,12 +72,41 @@ def test_gen_patterns():
         (r'\[(\d+)\](\w{0,2}K\w{0,2})\[(\d+)\]', 4),             # '--K--'
         (r'\[(\d+)\](\w{0,2}中\w{0,2})\[(\d+)\]', 8),            # '--中--'
         (r'\[(\d+)\](ß\w{0,2})\[(\d+)\]', 2),                    # 'ß--'
-
     ]
     assert set(patterns) == set(expected_patterns)
 
-    patterns = list(domain.gen_patterns('--中--', pos=0))
+    patterns = list(domain.gen_patterns('--中--', pos=0, min_size=5))
     expected_patterns = [
         (r'\[(\d+)\](\w{0,2}中\w{0,2})\[(\d+)\]', 0),            # '--中--'
     ]
     assert set(patterns) == set(expected_patterns)
+
+
+def tst_word():
+    word = domain.Word(word='Test')
+    word.set_position(row=1, col=2, direction=0)
+    assert word.letter_at(0,0) is None
+    assert word.letter_at(1,1) is None
+    assert word.letter_at(1,6) is None
+    assert word.letter_at(1,2) == 'T'
+    assert word.letter_at(1,3) == 'E'
+    assert word.letter_at(1,5) == 'T'
+
+    # TODO
+    assert word.blocked_span(padding=False) == (1,2,0)
+    assert word.blocked_span(padding=True) == (1,2,0)
+
+
+def test_puzzle():
+    puzzle = domain.Puzzle(grid_size=9, available_words=[('Word', 'clueWord'), ('WTester', 'clueWTester'), ('Sm','clueSm'), ('Tooooolonnnnnnnggg', '')])
+    assert puzzle.min_size_word == 2
+    assert puzzle.available_wordseq == '[0]WTESTER[1]WORD[2]SM'
+
+    # this expects at least one letter at row/col index
+    try:
+        puzzle.get_letter_sequences(direction=1, index=2)
+        assert False
+    except Exception:
+        pass
+
+    
